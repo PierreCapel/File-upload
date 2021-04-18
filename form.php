ecd9b61b4ab9922234e3bf659f8ee63fa1e9575b
PierreCapel <pecapel@gmail.com>
2021-04-18 14:06:00 +0200

    corrected files path

--- form.php
+++ form.php
@@ -1,19 +1,19 @@
 <?php
-$filePath='';
+$filePath = '';
 //check methode serveur
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     //set dossier recpeption
-    $uploadDir = '/home/pierre/Dev/Quests/upload-files/';
+    $uploadDir = __DIR__ . "/";
     //recup extension
     $extension = pathinfo($_FILES['profilePicture']['name'], PATHINFO_EXTENSION);
     //set chemin fichier et uniq id
-    $uploadFile = $uploadDir . uniqid() . "." . basename($_FILES['profilePicture']['name'])  ;
+    $uploadFile = $uploadDir . uniqid() . "." . basename($_FILES['profilePicture']['name']);
     //set liste d'extensions
     $extensionsOk = ['jpg', 'png', 'webp'];
     //set taille maxi en octets
     $maxFileSize = 1000000;
-    
-    
+
+
     //check extension
     if (!in_array($extension, $extensionsOk)) {
         echo 'Profile picture must be of type jpg png or webp';
@@ -21,22 +21,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         //check taille du fichier et si il existe
         if (file_exists($_FILES['profilePicture']['tmp_name']) && filesize($_FILES['profilePicture']['tmp_name']) > $maxFileSize) {
             echo "Maximum picture size is 1Mo";
-        }
-        else {
+        } else {
             //upload depuis repertoire temp vers chemin spécifié
             move_uploaded_file($_FILES['profilePicture']['tmp_name'], $uploadFile);
             //preparation du nom du fichier pour passage dans $post pour afficher sur la page profil
+            var_dump($uploadFile);
             $filePath = explode("/", $uploadFile);
             $filePath = end($filePath);
             echo 'Profile picture uploaded!';
-            }
-        }
-        //affiche message ok ou erreurs
-        if (empty($errors) && ($_FILES["profilePicture"]['error']) ===! 0) {
-            echo 'upload error number ' . $_FILES["profilePicture"]['error'];
         }
     }
-    $_POST['img'] = $filePath;
+    //affiche message ok ou erreurs
+    if (empty($errors) && ($_FILES["profilePicture"]['error']) === !0) {
+        echo 'upload error number ' . $_FILES["profilePicture"]['error'];
+    }
+}
+$_POST['img'] = $filePath;
 ?>
 <!--formulaire-->
 <p> Profile Picture :
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <p> Your first name <input type="text" name="firstname"></p>
     <p> Your last name <input type="text" name="lastname"></p>
     <p> Your age <input type="text" name="age"></p>
-<!-- passage URL fichier dans POST-->
-    <input type="hidden" name='img-path' value="<?php echo $filePath?>">
+    <!-- passage URL fichier dans POST-->
+    <input type="hidden" name='img-path' value="<?php echo $filePath ?>">
     <button name="submit">Submit profile</button>
 </form>
